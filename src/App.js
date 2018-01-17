725d0bdc21129b3919a3af11d1d7ffbd98d87e3b
heero7 <kevin7.mudia@gmail.com>
2018-01-17 12:35:00 -0600

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import React, { Component } from 'react';
 
-import { connect } from 'react-redux';
-
-import * as actions from './actions';
+// CONTAINERS
+import MoviesList from './containers/movies-list';
 
 class App extends Component {
 
-  componentWillMount() {
-    this.props.moviesList();
-  }
 
-  /// comes in as null
-  // check if movies is null or not!
-  renderList = (movies) => {
-    if (movies) {
-      return movies.map((movie, i) => {
-        return (
-          <div key={i}>{movie.name}</div>
-        )
-      });
-    }
-  }
-
+ 
   render() {
     return (
       <div>
-        {this.renderList(this.props.movies)}
+        <MoviesList />
       </div>
     );
   }
 }
 
-function mapStateToProps(state){
-  console.log(state);
-  return {
-    movies: state.movies
-  }
-}
-
-//export default App;
-export default connect(mapStateToProps, actions)(App);
+export default App;
---
+++ src/containers/movies-list.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
+
+class MoviesList extends Component {
+
+  componentWillMount() {
+    this.props.moviesList();
+  }
+
+  /// comes in as null
+  // check if movies is null or not!
+  renderList = (movies) => {
+    if (movies) {
+      return movies.map((movie, i) => {
+        return (
+          <div key={i}>{movie.name}</div>
+        )
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderList(this.props.movies)}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state){
+  console.log(state);
+  return {
+    movies: state.movies
+  }
+}
+
+//export default MoviesList;
+export default connect(mapStateToProps, actions)(MoviesList);
